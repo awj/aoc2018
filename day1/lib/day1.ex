110c99b965dbaf7b2cc390e9007cbfeed9d17ac5
Adam Jones <ajones1@gmail.com>
2018-12-02 21:27:30 -0700

    Day1 solved

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -1,18 +1,38 @@
 defmodule Day1 do
-  @moduledoc """
-  Documentation for Day1.
-  """
+  def frequency(list, initial \\ 0) do
+    Enum.reduce(list, initial, fn x, acc -> acc + x end)
+  end
 
-  @doc """
-  Hello world.
+  def repeat_check(changes, freq \\ 0, seen \\ %{}) do
+    case check(changes, freq, seen) do
+      {:uniq, freq, seen} -> repeat_check(changes, freq, seen)
+      {:dup, new_freq} -> new_freq
+    end
+  end
 
-  ## Examples
+  def evaluate(freq, change, seen) do
+    new_freq = freq + change
+    case Map.get(seen, new_freq) do
+      1 -> { :dup, new_freq }
+      _ -> { :uniq, new_freq }
+    end
+  end
 
-      iex> Day1.hello()
-      :world
+  def check([], freq, seen) do
+    {:uniq, freq, seen}
+  end
 
-  """
-  def hello do
-    :world
+  def check([change | rest], freq, seen) do
+    case evaluate(freq, change, seen) do
+      {:dup, new_freq } -> {:dup, new_freq }
+      {:uniq, new_freq } -> check(rest, new_freq, Map.put(seen, new_freq, 1))
+    end
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(fn(x) -> elem(x,0) end)
   end
 end
