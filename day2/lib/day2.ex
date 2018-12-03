30b0047a11b7b472fa54f8382b398183cfb20ea4
Adam Jones <ajones1@gmail.com>
2018-12-02 22:04:02 -0700

    Day2 complete.

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -17,4 +17,39 @@ defmodule Day2 do
       _ -> state
     end
   end
+
+  def similarity(a, b) do
+    Enum.zip(a,b)
+    |> Enum.count(fn({x,y}) -> x != y end)
+  end
+
+  def compare([]) do
+    :far
+  end
+
+  def compare([head | inputs]) do
+    case most_similar(head, inputs) do
+      {:close, other } -> {head, other}
+      :far -> compare(inputs)
+    end
+  end
+
+  def most_similar(check, inputs) do
+    {count, _, result} = inputs
+    |> Enum.reduce({10000, check, nil}, &digest_similarity/2)
+
+    case count do
+      1 -> {:close, result}
+      _ -> :far
+    end
+  end
+
+  def digest_similarity(x, {count, check, old}) do
+    similarity = Day2.similarity(check, x)
+
+    cond do
+      similarity < count -> {similarity, check, x}
+      true -> {count, check, old}
+    end
+  end
 end
